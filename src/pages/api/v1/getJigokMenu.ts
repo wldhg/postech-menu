c4df962fdd068399153047f8cf0128db4e66e2c8
Jio <jio@wldh.org>
2020-03-01 21:07:24 +0900

    Jigok API supports no meal day.

--- src/pages/api/v1/getJigokMenu.ts
+++ src/pages/api/v1/getJigokMenu.ts
@@ -29,6 +29,19 @@ const jigokTimeout = {
   },
 };
 
+const jigokNoMenu = {
+  ko: {
+    breakfast: ['식사가 없는 날입니다.'],
+    lunch: ['식사가 없는 날입니다.'],
+    dinner: ['식사가 없는 날입니다.'],
+  },
+  en: {
+    breakfast: ['There\'re no meals today.'],
+    lunch: ['There\'re no meals today.'],
+    dinner: ['There\'re no meals today.'],
+  },
+};
+
 const jigokMenu = {
   en: {
     breakfast: [],
@@ -91,61 +104,69 @@ const getJigokMenu = (I: http.IncomingMessage, O: http.OutgoingMessage) => {
           const body = cheerio.load(raw);
           body('.list_td tr').each((ri, tr) => {
             if (ri === 3) {
-              // Define parsing function
-              const parse = (td) => {
-                const text: string = td.text();
-                let parsed = [text];
-                if (text.indexOf('"') === 0) {
-                  parsed = text.substring(1, text.length - 1).split('"\n"')
-                    .map((item) => item.split('\n')[locale === 'ko' ? 0 : 1]);
-                } else if (text.indexOf('\n') === -1) {
-                  if (locale === 'ko') {
-                    parsed = [text.substring(0, text.indexOf(' '))];
-                  } else {
-                    parsed = [text.substring(text.indexOf(' '))];
+              if (body(tr).text().indexOf('등록된 메뉴가 없습니다') > -1) {
+                jigokMenu[locale] = jigokNoMenu[locale];
+                jigokRet[locale] = JSON.stringify(jigokMenu[locale]);
+                jigokDate = moment().format('YYYYMMDD');
+                jigokParsing = false;
+                O.end(jigokRet[locale]);
+              } else {
+                // Define parsing function
+                const parse = (td) => {
+                  const text: string = td.text();
+                  let parsed = [text];
+                  if (text.indexOf('"') === 0) {
+                    parsed = text.substring(1, text.length - 1).split('"\n"')
+                      .map((item) => item.split('\n')[locale === 'ko' ? 0 : 1]);
+                  } else if (text.indexOf('\n') === -1) {
+                    if (locale === 'ko') {
+                      parsed = [text.substring(0, text.indexOf(' '))];
+                    } else {
+                      parsed = [text.substring(text.indexOf(' '))];
+                    }
                   }
-                }
-                return parsed;
-              };
-              const getParsed = (type, btd) => {
-                /* eslint-disable no-else-return */
-                const td = btd[0];
-                if (td.attribs && td.attribs.colspan && td.attribs.colspan === '2') {
-                  // One Menu
-                  jigokMenu[locale][type] = parse(btd);
-                  return true;
-                } else if (Object.keys(jigokMenu[locale][type]).length === 0) {
-                  // Two Menu A
-                  jigokMenu[locale][type].push(locale === 'ko' ? '== A 코너' : '== A Course');
-                  jigokMenu[locale][type].push(...parse(btd));
-                  return false;
-                } else {
-                  // Two Menu B
-                  jigokMenu[locale][type].push(locale === 'ko' ? '== B 코너' : '== B Course');
-                  jigokMenu[locale][type].push(...parse(btd));
-                  return true;
-                }
-              };
-              // Parse
-              let breakfastCompleted = false;
-              let lunchCompleted = false;
-              let dinnerCompleted = false;
-              const tds = body(tr).find('td');
-              for (let di = 0; di < tds.length; di += 1) {
-                const btd = body(tds[di]);
-                if (!breakfastCompleted) {
-                  breakfastCompleted = getParsed('breakfast', btd);
-                } else if (!lunchCompleted) {
-                  lunchCompleted = getParsed('lunch', btd);
-                } else {
-                  dinnerCompleted = getParsed('dinner', btd);
-                }
-                if (dinnerCompleted) {
-                  clearTimeout(timeout);
-                  jigokRet[locale] = JSON.stringify(jigokMenu[locale]);
-                  jigokDate = moment().format('YYYYMMDD');
-                  jigokParsing = false;
-                  O.end(jigokRet[locale]);
+                  return parsed;
+                };
+                const getParsed = (type, btd) => {
+                  /* eslint-disable no-else-return */
+                  const td = btd[0];
+                  if (td.attribs && td.attribs.colspan && td.attribs.colspan === '2') {
+                    // One Menu
+                    jigokMenu[locale][type] = parse(btd);
+                    return true;
+                  } else if (Object.keys(jigokMenu[locale][type]).length === 0) {
+                    // Two Menu A
+                    jigokMenu[locale][type].push(locale === 'ko' ? '== A 코너' : '== A Course');
+                    jigokMenu[locale][type].push(...parse(btd));
+                    return false;
+                  } else {
+                    // Two Menu B
+                    jigokMenu[locale][type].push(locale === 'ko' ? '== B 코너' : '== B Course');
+                    jigokMenu[locale][type].push(...parse(btd));
+                    return true;
+                  }
+                };
+                // Parse
+                let breakfastCompleted = false;
+                let lunchCompleted = false;
+                let dinnerCompleted = false;
+                const tds = body(tr).find('td');
+                for (let di = 0; di < tds.length; di += 1) {
+                  const btd = body(tds[di]);
+                  if (!breakfastCompleted) {
+                    breakfastCompleted = getParsed('breakfast', btd);
+                  } else if (!lunchCompleted) {
+                    lunchCompleted = getParsed('lunch', btd);
+                  } else {
+                    dinnerCompleted = getParsed('dinner', btd);
+                  }
+                  if (dinnerCompleted) {
+                    clearTimeout(timeout);
+                    jigokRet[locale] = JSON.stringify(jigokMenu[locale]);
+                    jigokDate = moment().format('YYYYMMDD');
+                    jigokParsing = false;
+                    O.end(jigokRet[locale]);
+                  }
                 }
               }
             }
