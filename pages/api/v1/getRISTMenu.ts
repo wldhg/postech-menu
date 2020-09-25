c5665d5913e280b250ec9f7ad3e8c01213ee40de
Jio <jio@wldh.org>
2020-09-25 17:20:05 +0900

    timeout is now 7sec

--- pages/api/v1/getRISTMenu.ts
+++ pages/api/v1/getRISTMenu.ts
@@ -26,7 +26,7 @@ const getRISTMenu = (I: http.IncomingMessage, O: http.OutgoingMessage) => {
     let timeouted = false;
     setTimeout(() => {
       timeouted = true;
-    }, 10000);
+    }, 7000);
     const checkInterval = setInterval(() => {
       if (Object.keys(ristMenu.dinner).length > 0) {
         O.end(ristRet);
